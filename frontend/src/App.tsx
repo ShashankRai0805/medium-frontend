116d327b78ef48427ea0f2b462da4aa75893a387
Shashank Rai <shashankrai0805@gmail.com>
2025-07-12 16:50:14 +0530

    Complete auth setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
-// import { Signin } from './pages/Signin'
 // import { Blog } from './pages/Blog'
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,13 +1,27 @@
 import type { SignupInput } from "medium-common-shashankrai";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+    const navigate = useNavigate();
     const [postInputs, setPostInputs] = useState<SignupInput>({
         name: "",
         email: "",
         password: ""
     })
+
+    async function sendRequest() {
+        try {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`, postInputs)
+            const jwt = response.data;
+            localStorage.setItem("token", jwt);
+            navigate("/blogs")
+        } catch (error) {
+
+        }
+    }
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
             <div>
@@ -17,7 +31,7 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
                     </div>
                     <div className="text-slate-500">
                         {type === "signin" ? "Don't have an account?" : "Already have an account?"}
-                        <Link className="pt-2 underline" to={type === "signin" ? "/signup" : "/signin"}>{type === "signin" ? "Sign up": "Signin"}</Link>
+                        <Link className="pt-2 underline" to={type === "signin" ? "/signup" : "/signin"}>{type === "signin" ? "Sign up" : "Signin"}</Link>
                     </div>
                 </div>
                 <div className="pt-4">
@@ -36,11 +50,11 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
                     <LabelledInput label="Password" type={"password"} placeholder="123456" onChange={(e) => {
                         setPostInputs({
                             ...postInputs,
-                            name: e.target.value
+                            password: e.target.value
                         })
                     }} />
 
-                    <button type="button" className="w-full mt-8 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up": "Sign in"}</button>
+                    <button onClick={sendRequest} type="button" className="w-full mt-8 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
                 </div>
             </div>
 
