e1027003e7af3b844734799aa5260f00fda63859
Shashank Rai <shashankrai0805@gmail.com>
2025-07-12 16:06:09 +0530

    Made the signin and signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Signup } from './pages/Signup'
+import { Signin } from './pages/Signin'
 // import { Signin } from './pages/Signin'
 // import { Blog } from './pages/Blog'
 
@@ -10,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />} />
-          {/* <Route path="/signin" element={<Signin />} /> */}
+          <Route path="/signin" element={<Signin />} />
           {/* <Route path="/blog/:id" element={<Blog />} /> */}
         </Routes>
       </BrowserRouter>
