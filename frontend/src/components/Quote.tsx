4ac94459e6dc2800d155e5f3a1460ba8bb94b004
Shashank Rai <shashankrai0805@gmail.com>
2025-07-12 14:54:37 +0530

    Made the basic frontedn boiler plat

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+
+
+export const Quote: React.FC = () => {
+    return <div className="bg-slate-100 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-3xl font-bold">
+                    "The customer support I received was exceptional;. The support team went above and beyond to address my concerns"
+                </div>
+                <div className="max-w-wd text-xl font-semibold mt-4">
+                    John Doe,
+                </div>
+                <div className="max-w-wd text-sm font-semibold text-slate-400">
+                    CEO of Example Corp
+                </div>
+            </div>
+        </div>
+    </div>
+};
